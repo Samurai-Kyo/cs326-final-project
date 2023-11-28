ec555bf5367bae59a45b11339875f5780299b455
Rubio <jonrubio92@gmail.com>
2023-11-28 14:27:31 -0500

    Added static frontend files to backend.

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use('/', express.static('client'));
 app.use('/', router);
+
+app.all('*', async (request, response) => {
+    response.status(404).send(`Not found: ${request.path}`);
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
