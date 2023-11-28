6fff1c558d7b08839c7ef2d5cd4b5c077a3476b5
Rubio <jonrubio92@gmail.com>
2023-11-28 14:21:49 -0500

    Added express, morgan for backend. Set up express app to use router.

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import cors from 'cors';
 import morgan from 'morgan';
+import router from './routes/route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(morgan('dev'));
+app.use(express.json());
 
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
